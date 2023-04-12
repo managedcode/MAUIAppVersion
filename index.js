751de5326ca2482abd48bfe93ee71c359b2d133a
Konstantin Semenenko <mail@ksemenenko.com>
2023-04-13 00:04:20 +0200

    js

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+    const fs = require('fs');
+
+    const csproj = core.getInput('csproj');
+    const version = core.getInput('version');
+    const displayVersion = core.getInput('displayVersion');
+    const printFile = core.getInput('printFile');
+    
+
+    // match <ApplicationVersion> followed by any sequence of characters that are not a '<', followed by </ApplicationVersion>
+    const applicationVersionPattern = /<ApplicationVersion>[^<]*<\/ApplicationVersion>/g; 
+    const applicationDisplayVersionPattern = /<ApplicationDisplayVersion>[^<]*<\/ApplicationDisplayVersion>/g; 
+    
+    if (version && version.trim() !== '') {
+        // Read the file contents
+        var fileContents = fs.readFileSync(csproj, 'utf8');
+        var updatedApplicationVersion = fileContents.replace(applicationVersionPattern, `<ApplicationVersion>${version}</ApplicationVersion>`);
+        // Write the updated contents back to the file
+        fs.writeFileSync(csproj, updatedApplicationVersion, 'utf8');
+    } 
+
+    if (displayVersion && displayVersion.trim() !== '') {
+        // Read the file contents
+        var fileContents = fs.readFileSync(csproj, 'utf8');
+        var updatedApplicationVersion = fileContents.replace(applicationDisplayVersionPattern, `<ApplicationDisplayVersion>${displayVersion}</ApplicationDisplayVersion>`);
+        // Write the updated contents back to the file
+        fs.writeFileSync(csproj, updatedApplicationVersion, 'utf8');
+    }
+
+    if(printFile) {
+        var fileContents = fs.readFileSync(csproj, 'utf8');
+        console.log(fileContents);
+    }
+} 
+catch (error) {
+    core.setFailed(error.message);
+}
